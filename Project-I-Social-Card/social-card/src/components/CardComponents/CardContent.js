8b30b6bce3151df1a2d215aca7152907c405e5eb
mcferrenm <mcferrenmax@gmail.com>
2018-12-17 16:19:42 -0500

    add CardContent component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div>
+      <h4>Get Started with React</h4>
+      <span>React makes it painless to create interactive UIs. Design simple views for each state in your application.</span>
+      <span>reactjs.org</span>
+    </div>
+  );
+};
+
+export default CardContent;
