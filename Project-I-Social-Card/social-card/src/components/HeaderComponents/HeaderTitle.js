f70b9cf443a1b76675bd50dc076fc0dab6c38ff8
mcferrenm <mcferrenmax@gmail.com>
2018-12-17 15:56:55 -0500

    add HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h4>Lambda School</h4>
+      <span>@LambdaSchool</span>
+      <span>{Date.now()}</span>
+    </div>
+  );
+};
+
+export default HeaderTitle;
