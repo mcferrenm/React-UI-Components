08c01f94d1d896a846c6a969d7dcd962b2034c5e
mcferrenm <mcferrenmax@gmail.com>
2019-01-07 17:09:15 -0500

    Add Card content
    Added react logo banner, and content text without styles, then tested.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./Card.css";
 
 const CardBanner = () => {
-  return <div>CardBanner</div>;
+  return (
+    <img
+      src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+      alt="React Logo Banner"
+    />
+  );
 };
 
 export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Card.css";
 
 const CardContent = () => {
-  return <div>CardContent</div>;
+  return (
+    <div>
+      <h2>Get started with React</h2>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.
+      </p>
+      <a href="#">reactjs.org</a>
+    </div>
+  );
 };
 
 export default CardContent;
