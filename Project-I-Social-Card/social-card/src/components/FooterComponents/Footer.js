2b1a3533518354651ccb84d3ca374361f56848c9
mcferrenm <mcferrenmax@gmail.com>
2019-01-07 18:55:00 -0500

    Add icons to footer
    Imported font-awesome library to index.html, styled icons as well.

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import "./Footer.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <i className="far fa-comment" />
+      <i className="fas fa-retweet">6</i>
+      <i className="far fa-heart">4</i>
+      <i className="far fa-envelope" />
+    </div>
+  );
 };
 
 export default Footer;
