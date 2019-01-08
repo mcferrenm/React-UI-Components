d48500361ef920d60e1221e29d15baf101b296e3
mcferrenm <mcferrenmax@gmail.com>
2019-01-08 17:08:05 -0500

    Change App Component to class component
    Import React.Component as Component and extend App from it. Add
    constructor and add a class variable this.state

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
 import ActionButton from "./components/ButtonComponents/ActionButton";
@@ -6,91 +6,100 @@ import NumberButton from "./components/ButtonComponents/NumberButton";
 
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="app">
-      <CalculatorDisplay />
-      <ActionButton
-        value="clear"
-        style={{
-          container: "clear-button-container",
-          text: "clear-button-text"
-        }}
-      />
-      <ActionButton
-        value="÷"
-        style={{
-          container: "action-button-container",
-          text: "action-button-text"
-        }}
-      />
-      <NumberButton
-        value="7"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <NumberButton
-        value="8"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <NumberButton
-        value="9"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <ActionButton
-        value="✕"
-        style={{
-          container: "action-button-container",
-          text: "action-button-text"
-        }}
-      />
-      <NumberButton
-        value="4"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <NumberButton
-        value="5"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <NumberButton
-        value="6"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <ActionButton
-        value="–"
-        style={{
-          container: "action-button-container",
-          text: "action-button-text"
-        }}
-      />
-      <NumberButton
-        value="1"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <NumberButton
-        value="2"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <NumberButton
-        value="3"
-        style={{ container: "non-zero-button-container" }}
-      />
-      <ActionButton
-        value="+"
-        style={{
-          container: "action-button-container",
-          text: "action-button-text"
-        }}
-      />
-      <NumberButton value="0" style={{ container: "zero-button-container" }} />
-      <ActionButton
-        value="="
-        style={{
-          container: "action-button-container",
-          text: "action-button-text"
-        }}
-      />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="app">
+        <CalculatorDisplay />
+        <ActionButton
+          value="clear"
+          style={{
+            container: "clear-button-container",
+            text: "clear-button-text"
+          }}
+        />
+        <ActionButton
+          value="÷"
+          style={{
+            container: "action-button-container",
+            text: "action-button-text"
+          }}
+        />
+        <NumberButton
+          value="7"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <NumberButton
+          value="8"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <NumberButton
+          value="9"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <ActionButton
+          value="✕"
+          style={{
+            container: "action-button-container",
+            text: "action-button-text"
+          }}
+        />
+        <NumberButton
+          value="4"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <NumberButton
+          value="5"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <NumberButton
+          value="6"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <ActionButton
+          value="–"
+          style={{
+            container: "action-button-container",
+            text: "action-button-text"
+          }}
+        />
+        <NumberButton
+          value="1"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <NumberButton
+          value="2"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <NumberButton
+          value="3"
+          style={{ container: "non-zero-button-container" }}
+        />
+        <ActionButton
+          value="+"
+          style={{
+            container: "action-button-container",
+            text: "action-button-text"
+          }}
+        />
+        <NumberButton
+          value="0"
+          style={{ container: "zero-button-container" }}
+        />
+        <ActionButton
+          value="="
+          style={{
+            container: "action-button-container",
+            text: "action-button-text"
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
